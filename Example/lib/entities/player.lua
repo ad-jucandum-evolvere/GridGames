7900ac4d13e8bec4ad658d9b778d565846b2db1f
Srihari C.S. <22188422+Srihari-CS@users.noreply.github.com>
2025-02-05 00:06:12 +0530

    implemented example with classic game snake movement

---
+++ Example/lib/entities/player.lua
@@ -0,0 +1,69 @@
+Player = Object:extend()
+
+---constructor
+---@param x number
+---@param y number
+---@param width number
+---@param height number
+function Player:new(x, y, width, height)
+    self.x = x or 0
+    self.y = y or 0
+    self.width = width or 10
+    self.height = height or 10
+    self.direction = 1
+    self.speed = 50
+    self.semiWidth = self.width / 2
+    self.semiHeight = self.height / 2
+    self.colorRadians = 0
+end
+
+function Player:update(dt)
+    if self.direction == 1 then
+        self.x = self.x - self.speed * dt
+    elseif self.direction == 2 then
+        self.y = self.y - self.speed * dt
+    elseif self.direction == 3 then
+        self.x = self.x + self.speed * dt
+    elseif self.direction == 4 then
+        self.y = self.y + self.speed * dt
+    end
+
+    -- handle warping
+    local screenWidth = love.graphics.getWidth()
+    local screenHeight = love.graphics.getHeight()
+    if self.x + self.semiWidth <= 0 then
+        self.x = screenWidth + self.semiWidth
+    elseif self.x - self.semiWidth >= screenWidth then
+        self.x = -self.semiWidth
+    end
+    if self.y + self.semiHeight <= 0 then
+        self.y = screenHeight + self.semiHeight
+    elseif self.y - self.semiHeight >= screenHeight then
+        self.y = -self.semiHeight
+    end
+    self.colorRadians = self.colorRadians + 0.1
+end
+
+function Player:draw()
+    local left = self.x - self.semiWidth
+    local top = self.y - self.semiHeight
+
+    local red = 1 + math.sin(self.colorRadians + math.pi / 2)
+    local green = 1 + math.sin(self.colorRadians + math.pi)
+    local blue = 1 + math.sin(self.colorRadians)
+
+    love.graphics.setColor(red, green, blue)
+    love.graphics.rectangle("fill", left, top, self.width, self.height)
+    love.graphics.setColor(1, 1, 1)
+end
+
+function Player:handleKeyPress(key)
+    if key == "escape" then love.event.quit(0) end
+    if key == "up" or key == "left" then
+        self.direction = self.direction - 1
+        if self.direction == 0 then self.direction = 4 end
+    elseif key == "down" or key == "right" then
+        self.direction = self.direction + 1
+        if self.direction == 5 then self.direction = 1 end
+    end
+end
--- Example/main.lua
+++ Example/main.lua
@@ -1,7 +1,23 @@
-function love.draw()
-    love.graphics.print("Hello World", 400, 300)
+if arg[2] == "debug" then
+    require("lldebugger").start()
 end
 
-function lasds()
-    print("Hellow World")
+function love.load()
+    Object = require("lib.vendor.classic")
+    require("lib.entities.player")
+
+    love.window.setTitle("Example")
+    player = Player(love.graphics.getWidth() / 2, love.graphics.getHeight() / 2, 20, 20)
+end
+
+function love.update(dt)
+    player:update(dt)
+end
+
+function love.draw()
+    player:draw()
+end
+
+function love.keypressed(key)
+    player:handleKeyPress(key)
 end
